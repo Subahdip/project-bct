b7111eff6db12e5f7f1f68a81c412a5eb1b6d5f9
subhajit-10m <subhajitdey1020@gmail.com>
2025-07-25 12:23:14 +0530

    add s-10

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,14 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>Hello world subhajit </h1>
-=======
-      <h1>Hello world subhdip</h1>
->>>>>>> ff41d0492173992696c70ced2da3a883e1cad7a8
+      <h1>Hello world subhajit</h1>
     </div>
   )
 }
 
 export default App
-
