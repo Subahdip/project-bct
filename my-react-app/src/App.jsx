725d0e9343fdc6985dd5f2130df5b53faed5d392
subhajit-10m <subhajitdey1020@gmail.com>
2025-07-25 12:16:42 +0530

    subhajit name added

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello world subhjit</h1>
+      <h1>Hello world subhajit </h1>
     </div>
   )
 }
 
 export default App
+
